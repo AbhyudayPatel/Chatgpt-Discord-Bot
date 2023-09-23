b064062a91701b1dda20cc4dc63c94ba913e6494
Abhyuday Patel <137634206+AbhyudayPatel@users.noreply.github.com>
2023-09-23 14:41:44 +0530

    updates

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+token = "MTEzMzk3ODMzMTI0MzYxMDE1Mg.G_UO8p.-Z2oZEh8zk6Z7_H2g5XmIwb2a8Ef3vwVQxOy0U"
+
+class MyClient(discord.Client):
+
+  async def on_ready(self):
+    print(f'Logged on as {self.user}!')
+
+  async def on_message(self, message):
+    print(f'Message from {message.author}: {message.content}')
+    print(message.mentions)
+    if self.user != message.author:
+      if self.user in message.mentions:
+        channel=message.channel
+        response = openai.Completion.create(model="text-davinci-003",
+                                            prompt=message.content,
+                                            temperature=1,
+                                            max_tokens=256,
+                                            top_p=1,
+                                            frequency_penalty=0,
+                                            presence_penalty=0)
+        messagetosend = response.choices[0].text
+        await channel.send(messagetosend)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(token)
